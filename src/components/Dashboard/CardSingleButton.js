7f1e05aea50596f3cc83dfbc9887dba4a04b1179
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-02-27 23:21:55 +0530

    Modify cards

--- src/components/Dashboard/CardSingleButton.js
+++ src/components/Dashboard/CardSingleButton.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 
 
-export default function CardElement({title, text, buttonText}) {
+export default function CardSingleButton({title, text, buttonText}) {
   return (
     <>
-    <Card className= "dashboard-card text-center" style={{ width: "18rem"}} >
+    <Card className= "dashboard-card text-center" >
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{text}</Card.Text>
         </Card.Body>
-        <Button className= "button my-auto" variant="primary">{buttonText}</Button>
+        <Button className= "button my-auto" variant="primary" >{buttonText} </Button>
       
     </Card>
     </>
