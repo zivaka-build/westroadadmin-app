05ba5e534ae953ee282db777ea5076e77e0e52a4
AdamParker19 <anmolpatel668@gmail.com>
2021-02-27 18:05:36 +0530

    added Error 404 page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Router } from "@reach/router"
 import "./assets/css/style.css"
 import LoginPage from "./LoginPage.js"
 import 'animate.css';
-
+import NotFound from './NotFound.js'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <LoginPage path="/" />
-
+        <NotFound path="/notfound" />
 
 
       </Router>
