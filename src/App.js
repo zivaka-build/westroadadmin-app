3b7724abbe66ff555c667899af8a5e99d1ad2d79
Subhradeep Das <dassubhra1998@gmail.com>
2021-03-05 18:07:53 +0530

    Dashboard Added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./assets/css/style.css"
 import LoginPage from "./LoginPage.js"
 import 'animate.css';
 import NotFound from './NotFound.js'
+import Dashboard from "./../src/components/Dashboard/Dashboard.js"
 
 function App() {
   return (
     <div className="App">
       <Router>
         <LoginPage path="/" />
+        <Dashboard path="/dashboard">
         <NotFound path="/notfound" />
-
+        </Dashboard>
 
       </Router>
     </div>
---
+++ src/components/Test/Test1.js
@@ -0,0 +1,12 @@
+import React, { Component, useEffect, useState } from "react"
+
+const Test1 = () => {
+    return(
+        <div>
+            <center>
+                <h2>Test 1</h2>
+            </center>
+        </div>
+    );
+
+}
