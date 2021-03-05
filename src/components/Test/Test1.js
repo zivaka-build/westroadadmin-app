61d3af300aeee390ff48df13333ca5830f6672f5
Subhradeep Das <dassubhra1998@gmail.com>
2021-03-05 16:33:46 +0530

    Issue fixed

---
+++ src/components/Test/Test1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Test1 = () => {
+    return(
+        <div>
+            <center>
+                <h2>Test 1</h2>
+            </center>
+        </div>
+        
+    );
+}
+
+export default Test1;
