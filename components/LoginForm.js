808d1089909ba525f5460683412365a5bbe89a27
Subhradeep Das <65084523+subhradeep2198@users.noreply.github.com>
2021-03-05 16:47:50 +0530

    Revert "West 55"

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,46 +1,31 @@
-import React, { Component, useEffect, useState } from "react"
+import React, { Component } from "react"
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { navigate } from "@reach/router";
-<<<<<<< HEAD:src/components/Login/LoginForm.js
-import { BASE_URL } from '../../config/url.js';
-import WestRoad from '../../westroad.png';
-import zivaka from '../../zivaka.png';
-import base from '../../BASE.png';
-import Swal from 'sweetalert2';
-import qs from 'qs';
-import westroad from '../../westroad_logo-02.jpg';
-import { Form, Input } from 'reactstrap';
-import rect from '../../Rectangle.png';
-
-function LoginForm() {
-=======
 import { BASE_URL } from './../config/url';
 import WestRoad from '../westroad.png';
 import zivaka from '../zivaka.png';
 import base from '../BASE.png';
 
 class LoginForm extends Component {
->>>>>>> origin:components/LoginForm.js
 
-  const [email, setemail] = useState('')
-  const [password, setPassword] = useState('')
-  const [userName, setuserName] = useState('')
+  state = {
+    brokerID: '',
+    password: '',
+    email: '',
+    baseurl: 'https://ilead.vaanic.com'
 
-  const changeUser = (e) => {
-    setuserName(e.target.value)
 
   }
 
-  const changePassword = (e) => {
-    setPassword(e.target.value)
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+
   }
 
-  const changemail = (e) => {
-    setemail(e.target.value)
-  }
-
-  const forgotPassword = (e) => {
+  forgotPassword = (e) => {
     var login = document.querySelector("#login");
     login.style.display = "none";
 
@@ -48,7 +33,7 @@ class LoginForm extends Component {
     forgotpassword.style.display = "block";
   }
 
-  const back = (e) => {
+  back = (e) => {
     var login = document.querySelector("#login");
     login.style.display = "block";
 
@@ -56,31 +41,21 @@ class LoginForm extends Component {
     forgotpassword.style.display = "none";
   }
 
-  const resetLink = (e) => {
+  resetLink = (e) => {
+    const user = {
+      email: this.state.email
+    }
     e.preventDefault()
 
-    const user = {
-      email: email
+    if (document.reset.email.value == "") {
+      var error = document.querySelector("#error");
+      error.style.display = "block";
     }
-    if (email === '') {
-      Swal.fire({
-        icon: 'error',
-        title: 'Ooops',
-        showClass: {
-          popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
-        },
-        text: 'email cannot be left empty!'
-      })
-    }
-    else {
-      const data = {
-        email: email,
-      }
 
-      axios.post(`${BASE_URL}` + '', qs.stringify(data))
+    else {
+
+      axios
+        .post(`${BASE_URL}` + '/api/v1.0/broker/forgotpassword', { email: user.email })
         .then((response) => {
           console.log(response)
           var message = document.querySelector("#message");
@@ -94,10 +69,9 @@ class LoginForm extends Component {
 
         })
     }
+
   }
 
-<<<<<<< HEAD:src/components/Login/LoginForm.js
-=======
   submitHandler = (e) => {
     const user = {
       brokerID: this.state.brokerID,
@@ -126,206 +100,124 @@ class LoginForm extends Component {
 
       )
 
->>>>>>> origin:components/LoginForm.js
 
-  const submitHandler = (e) => {
-
-    e.preventDefault()
-
-    if (email === '' || password === '') {
-      Swal.fire({
-        icon: 'error',
-        title: 'Ooops',
-        showClass: {
-          popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
-        },
-        text: 'email and password cannot be left empty!'
-      })
-    }
-    else {
-      const data = {
-        userName: userName,
-        password: password,
-      }
-      axios
-        .post('http://52.66.99.255:8050/api/v1/user/login', qs.stringify(data))
-        .then((response) => {
-          console.log(response);
-          if (response.status == 200) {
-            // Cookies.set('Token', response.data.token)
-            //  Cookies.set('FirstName', response.data.firstName)
-            // Cookies.set('email', response.data.email)
-            // Cookies.set('SuperUser', response.data.isSuperuser)
-            //  navigate("/tbro")
-            Swal.fire({
-              icon: 'error',
-              title: 'Ooops',
-              showClass: {
-                popup: 'animate__animated animate__fadeInDown'
-              },
-              hideClass: {
-                popup: 'animate__animated animate__fadeOutUp'
-              },
-              text: 'Login successful'
-            })
-          }
-          else {
-            Swal.fire({
-              icon: 'error',
-              title: 'Ooops',
-              showClass: {
-                popup: 'animate__animated animate__fadeInDown'
-              },
-              hideClass: {
-                popup: 'animate__animated animate__fadeOutUp'
-              },
-              text: response.message
-            })
-          }
-
-        })
-        .catch((error) => {
-          console.log(error);
-          Swal.fire({
-            icon: 'error',
-            title: 'Ooops',
-            showClass: {
-              popup: 'animate__animated animate__fadeInDown'
-            },
-            hideClass: {
-              popup: 'animate__animated animate__fadeOutUp'
-            },
-            text: error.message
-          })
-        }
-
-        )
-
-    }
 
   }
 
 
-  return (
-    //jsx-a11y/anchor-is-valid
-    <div className="bg-gradient-primary align-items-center">
+  render() {
+    return (
+      //jsx-a11y/anchor-is-valid
+      <div className="bg-gradient-primary align-items-center">
 
-      <div className="container" style={{ height: "100vh" }}>
-        {/* Outer Row */}
-        <div>
-          <img src={westroad} style={{ height: '260px', marginTop: '-90px' }} />
-        </div>
-        <div className="row pt-5 justify-content-center" style={{
-          position: 'fixed',
-          width: '500px',
-          right: '-10px',
-          top: '-10px'
-        }}>
+        <div className="container" style={{ height: "100vh" }}>
+          {/* Outer Row */}
 
-          <div className="col-xl-10 col-lg-12 col-md-9">
-            <div className="card o-hidden border-0 shadow-lg my-5">
-              <div className="card-body p-0" >
-                {/* Nested Row within Card Body */}
-                <div className="row">
+          <div className="row pt-5 justify-content-center" style={{
+            position: 'fixed',
+            width: '500px',
+            right: '-10px',
+            top: '-10px'
+          }}>
+            <div className="col-xl-10 col-lg-12 col-md-9">
+              <div className="card o-hidden border-0 shadow-lg my-5">
+                <div className="card-body p-0">
+                  {/* Nested Row within Card Body */}
+                  <div className="row">
 
-                  <div className="col-lg-12" id="login" name="login" style={{ display: "block" }}>
+                    <div className="col-lg-12" id="login" name="login" style={{ display: "block" }}>
 
-                    <div className="p-5" style={{ padding: '2.5rem' }}>
-                      <img src={WestRoad} style={{ height: '160px', marginLeft: '27%' }} />
+                      <div className="p-5" style={{ padding: '2.5rem' }}>
+                        <img src={WestRoad} style={{ height: '160px', marginBottom: '-15px', }} />
 
-                      <div className="text-center">
-                        <h1 className="h4 text-gray-900 mb-4">Westroad Management System</h1>
-                      </div>
-                      <Form className="user" onSubmit={submitHandler}>
-                        <div className="form-group">
-                          <Input type="text" className="form-control form-control-user" id="email" value={userName} name="email" onChange={changeUser} placeholder="Username" />
+                        <div className="text-center">
+                          <h1 className="h4 text-gray-900 mb-4">Westroad Management System</h1>
                         </div>
-                        <div className="form-group">
-                          <Input type="password" className="form-control form-control-user" id="password" value={password} name="password" onChange={changePassword} placeholder="Password (8+ characters)" />
-                        </div>
-                        <br />
+                        <form className="user" onSubmit={this.submitHandler}>
+                          <div className="form-group">
+                            <input type="text" className="form-control form-control-user" id="brokerID" name="brokerID" onChange={this.changeHandler} placeholder="Email address" />
+                          </div>
+                          <div className="form-group">
+                            <input type="password" className="form-control form-control-user" id="password" name="password" onChange={this.changeHandler} placeholder="Password (8+ characters)" />
+                          </div>
+                          <br />
 
-                        <button type="submit" href="" className="btn btn-primary btn-user btn-block" style={{
-                          background: "green",
-                          width: '200px',
-                          margin: '0 auto'
-                        }} >
-                          Login
+                          <button type="submit" href="" className="btn btn-primary btn-user btn-block" style={{
+                            background: "green",
+                            width: '200px',
+                            margin: '0 auto'
+                          }} >
+                            Login
                         </button>
 
 
-                      </Form>
+                        </form>
 
-                      <div className="text-center">
-                        <a className="small" onClick={forgotPassword}>Forgot Password?</a>
+                        <div className="text-center">
+                          <a className="small" onClick={this.forgotPassword}>Forgot Password?</a>
+                        </div>
+
                       </div>
-
                     </div>
-                  </div>
-                  <div className="col-lg-12" id="forgotpassword" name="forgotpassword" style={{ display: "none", }}>
-                    <div className="p-5">
-                      <img src={WestRoad} style={{ height: '160px', marginLeft: '10%' }} />
+                    <div className="col-lg-12" id="forgotpassword" name="forgotpassword" style={{ display: "none", }}>
+                      <div className="p-5">
+                        <img src={WestRoad} style={{ height: '160px', marginBottom: '-15px', }} />
 
-                      <div className="text-center">
-                        <h1 className="h4 text-gray-900 mb-4">Forgot Password</h1>
-                      </div>
-                      <form className="reset user" name="reset" id="reset" onSubmit={resetLink}>
-                        <div className="form-group">
-                          <input type="text" className="form-control form-control-user" type="email" id="email" name="email" value={email} onChange={changemail} placeholder="Please Enter Registered email" />
+                        <div className="text-center">
+                          <h1 className="h4 text-gray-900 mb-4">Forgot Password</h1>
                         </div>
+                        <form className="reset user" name="reset" id="reset" onSubmit={this.resetLink}>
+                          <div className="form-group">
+                            <input type="text" className="form-control form-control-user" type="email" id="email" name="email" onChange={this.changeHandler} placeholder="Please Enter Registered Email" />
+                          </div>
 
 
-                        <small name="message" id="message" style={{ display: 'none' }}>
-                          <em>Reset link sent to your mail. Please check!</em>
-                        </small>
-                        <small name="error" id="error" style={{ display: 'none', color: 'red' }}>
-                          <em>Please enter your email!</em>
-                        </small>
-                        <br />
+                          <small name="message" id="message" style={{ display: 'none' }}>
+                            <em>Reset link sent to your mail. Please check!</em>
+                          </small>
+                          <small name="error" id="error" style={{ display: 'none', color: 'red' }}>
+                            <em>Please enter your email!</em>
+                          </small>
+                          <br />
 
-                        <button type="submit" href="" className="btn btn-primary btn-user btn-block">
-                          Send Reset Link
+                          <button type="submit" href="" className="btn btn-primary btn-user btn-block">
+                            Send Reset Link
                         </button>
-                        <button className="btn btn-secondary btn-user btn-block" onClick={back}>
-                          Back To Login
+                          <button className="btn btn-secondary btn-user btn-block" onClick={this.back}>
+                            Back To Login
                         </button>
 
 
-                      </form>
+                        </form>
 
 
+                      </div>
                     </div>
                   </div>
                 </div>
+
               </div>
 
             </div>
             <img src={zivaka} style={{
-              height: '80px',
-              /* margin-top: 0%; */
-              position: 'absolute',
-              top: '85vh',
-              right: '5vh'
+              height: '80px', position: 'absolute',
+              top: '90vh',
+              right: '5vh',
             }} />
+
           </div>
 
-
         </div>
-
+        <img src={base} style={{
+          height: '130px',
+          position: 'absolute',
+          top: '82%',
+          right: '73%'
+        }} />
       </div>
-      <img src={base} style={{
-        height: '130px',
-        position: 'absolute',
-        top: '82%',
-        right: '73%'
-      }} />
-    </div>
 
-  );
+    );
+  }
 }
-
 
 export default LoginForm;
