7cb6a9f295fc85d4007e902b9b4029528d2fd958
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-03-13 01:48:27 +0530

    Render cards on dashboard

---
+++ src/components/Dashboard/Configuration.js
@@ -0,0 +1,24 @@
+import CardSingleButton from "./CardSingleButton";
+import React, { Component } from "react";
+import "../../assets/css/card-single-button.css";
+
+class Configuration extends Component {
+  render() {
+    return (
+      <div className="config" style={{ paddingTop: "70px" }}>
+        <div className="row">
+          <div className="col-6">
+            <CardSingleButton
+              title="Team Members"
+              text="Add, delete and view team members"
+              buttonText="Manage"
+              buttonLink="/dashboard/teammember"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Configuration;
