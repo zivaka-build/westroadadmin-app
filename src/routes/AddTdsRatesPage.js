e26de803844db4a9d7047e52de08d16545ef9b7f
Subhradeep Das <dassubhra1998@gmail.com>
2021-05-03 17:36:27 +0530

    Add TDS Rates

---
+++ src/routes/AddTdsRatesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import AddTdsRates from "../components/Dashboard/AddTdsRates.js";
+function AddTdsRatesPage() {
+  return <AddTdsRates />;
+}
+
+export default AddTdsRatesPage;
