773954a66c617bcfdd75930dd642f964565b762b
Subhradeep Das <dassubhra1998@gmail.com>
2021-05-06 20:45:56 +0530

    Individual Site Added

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import AddTdsRatesPage from "./routes/AddTdsRatesPage"
 import ViewTdsRatesPage from "./routes/ViewTdsRatesPage"
 import ViewTdsPage from "./routes/ViewTdsPage"
 import ViewSitePage from "./routes/ViewSitePage"
+import IndividualSitePage from "./routes/IndividualSitePage"
 
 function App() {
   return (
@@ -39,6 +40,7 @@ function App() {
           <ViewTdsRatesPage path="/viewtdsrates" />
           <ViewTdsPage path="/viewtds" />
           <ViewSitePage path="/managesite" />
+          <IndividualSitePage path="/individualsite/:siteID" />
         </DefaultDashboard>
 
       </Router>
