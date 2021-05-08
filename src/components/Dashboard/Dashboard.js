6c633b483e9917d879bb58154af77907f20363f8
Anindya Kumar Patro <anindya20012001@gmail.com>
2021-05-08 16:09:34 +0530

    List of Transaction page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -264,6 +264,12 @@ const drawer = (
                     <h7>Transaction List</h7>
                     </ListItem>
                     </Link>
+
+                    <Link style={{ color: '#073b4c', textDecoration: 'none'}} to='/dashboard/viewtds'>
+                    <ListItem button key={'TDS'}>
+                    <h7>TDS</h7>
+                    </ListItem>
+                    </Link>
                     </List>
                 
                 </AccordionDetails>
