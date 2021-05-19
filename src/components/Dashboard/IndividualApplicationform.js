a53e9ee0b22f3f01dbe5514beb80b0eeef786635
Subhradeep Das <dassubhra1998@gmail.com>
2021-05-20 02:43:48 +0530

    Payment Validation

--- src/components/Dashboard/IndividualApplicationform.js
+++ src/components/Dashboard/IndividualApplicationform.js
@@ -10,8 +10,41 @@ import Nav from 'react-bootstrap/Nav'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+}));
 
 function IndividualApplicationform() {
+    var date = new Date()
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const {applicationId} = useParams()
     const [ applicant, setApplicant ] = useState([])
@@ -68,9 +101,13 @@ function IndividualApplicationform() {
     const [cn, setCn] = useState("")
     const [ib, setIb] = useState("")
 
-    const [funded, setFunded] = useState(false)
+    const [funded, setFunded] = useState()
     const [fb, setFb] = useState("")
     const [fbp, setFbp] = useState("")
+
+    const [pv, setPv] = useState()
+    const [vb, setVb] = useState("")
+    const [vd, setVd] = useState("")
 
     const showApplicant = (e) => {
         if(disp === "none"){
@@ -142,6 +179,16 @@ function IndividualApplicationform() {
         })
 
     }
+
+    const validate = (e) => {
+      
+      axios.put(`${BASE_URL}/api/v1/payment/validatepayment`, {applicationId: applicationId, paymentValidatedBy: Cookies.get("FullName"), paymentValidatedDate: date })
+        .then(response=>{
+            console.log(response)
+            window.location.reload()
+          
+        })
+    }
     
       
     useEffect(()=>{
@@ -187,6 +234,17 @@ function IndividualApplicationform() {
             else if(response.data.notFundedSelf === false){
               setFunded(false)
               setFb("Self")
+            }
+
+            if(response.data.NEFTDetails || response.data.chequeDetails){
+              if(response.data.paymentValidated === true){
+                setPv(true)
+                setVb(response.data.paymentValidatedBy)
+                setVd(response.data.paymentValidatedDate)
+              }
+              else {
+                setPv(false)
+              }
             }
 
 
@@ -993,6 +1051,8 @@ function IndividualApplicationform() {
                 </div>
                   </>
                   : 
+
+                  funded === false ?
                   <>
                   <br />
                   <div className="row justify-content-center">
@@ -1011,7 +1071,87 @@ function IndividualApplicationform() {
                   </div>
                 </div>
                   </>
+                  : null
                 }
+                { 
+                pv === false ?
+                <>
+                <br />
+                <div className="row justify-content-center">
+                  <div className="col-8">
+                    <h5>Payment Validation</h5>
+                  </div>
+                </div>
+                <br />
+                <div className="row justify-content-center">
+                  <div className="col-4">
+                    <button className="btn btn-secondary btn-user" onClick={()=> setOpen(true)}>Validate Payment</button>
+                  </div>
+                </div>
+                </>
+                : 
+                pv === true ?
+                <>
+                <br />
+                <div className="row justify-content-center">
+                  <div className="col-8">
+                    <h5>Payment Validation</h5>
+                  </div>
+                </div>
+                <br />
+                <div className="row justify-content-center">
+                  <div className="col-4">
+                  <label>Validated By</label>
+                  <input
+                  type="text"
+                  class="form-control"
+                  value={vb}
+                  />
+                  </div>
+                  <div className="col-4">
+                  <label>Validate Date</label>
+                  <input
+                  type="text"
+                  class="form-control"
+                  value={vd.substring(8,10)+"-"+vd.substring(5,7)+"-"+vd.substring(0,4)}
+                  />
+                  </div>
+                </div>
+                </>
+                :
+                null
+                }
+                <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 500,
+                }}
+            >
+            <Fade in={open}>
+            <div className={classes.paper}>
+                <div className="row">
+                    <p>Are you sure you want to validate payment ?</p>
+                </div>
+                <div className="row container-fluid justify-content-center">
+                    <div className="col-4 text-right">
+                        <button className="btn btn-secondary btn-user" style={{backgroundColor: "white", color: "black"}} onClick={()=> setOpen(false)}>No</button>
+
+                    </div>
+                    &nbsp;&nbsp;
+                    <div className="col-4">
+                        <button className="btn btn-secondary btn-user" onClick={validate}>Yes</button>
+                                                    
+                    </div>
+                </div>
+            </div>
+            </Fade>
+            </Modal>
                 </Tab.Pane>
             </Tab.Content>
             </Col>
