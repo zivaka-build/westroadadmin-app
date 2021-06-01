f5ea900ea5087987ace6a4230306a7600cd89cf5
Subhradeep Das <dassubhra1998@gmail.com>
2021-06-01 20:10:25 +0530

    Changes

---
+++ src/components/Dashboard/IndividualApplicant.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react"
+import { useParams , navigate} from "@reach/router"
+import { Form } from "react-bootstrap";
+import { BASE_URL } from "../../config/url";
+import axios from "axios";
+import Cookies from "js-cookie";
+import {IoMdArrowBack} from 'react-icons/io'
+
+function IndividualApplicationform() {
+
+}
+
+export default IndividualApplicant;
